52b2a00c623064f9b92cf743b50208f23b965b86
Abdulaziz <abdurashidovabdulaziz1272@gmail.com>
2025-09-04 16:08:20 +0500

    sefa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import MainLogin from './features/Login/MainLogin';
+
+function App() {
+  return (
+    <>
+      <MainLogin/>
+    </>
+  );
+}
+
+export default App;
