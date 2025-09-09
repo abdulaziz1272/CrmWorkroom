0691b49f655e671e7d3726d90b28ec6fe36ae966
Abdulaziz <abdurashidovabdulaziz1272@gmail.com>
2025-09-09 14:43:28 +0500

    wgda

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import MainLogin from './features/Login/MainLogin';
+import ProjectsMain from './features/AddProjects/ProjectsMain';
 
 function App() {
   return (
     <>
-      <MainLogin/>
+      {/* <MainLogin/> */}
+      <ProjectsMain/>
     </>
   );
 }
